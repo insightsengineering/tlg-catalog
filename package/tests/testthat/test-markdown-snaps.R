fefa6d2309ad73613d1a55e47aa9cd3d0e250a9d
vedhav <vedhaviyash4@gmail.com>
2023-07-21 11:19:13 +0530

    revert: move back to package structure + change quarto list files logic

---
+++ package/tests/testthat/test-markdown-snaps.R
@@ -0,0 +1,28 @@
+quarto_docs <- list.files(
+  file.path("_book", c("tables", "listings")),
+  recursive = TRUE,
+  full.names = FALSE,
+  pattern = "*.qmd"
+)
+
+snapshot_docs <- fs::path_file(quarto_docs)
+snapshot_variants <- gsub("\\.qmd$", "", snapshot_docs)
+
+for (snapshot_variant in snapshot_variants) {
+  path <- file.path(test_data_path, paste0(snapshot_variant, ".rds"))
+  if (isFALSE(file.exists(path))) {
+    testthat::skip(paste0(
+      "Data snapshot file ", path,
+      " not found. Skipping tests for this table."
+    ))
+  }
+  data_snap <- readRDS(path)
+  for (i in names(data_snap)) {
+    testthat::test_that(i, {
+      testthat::expect_snapshot(
+        print(data_snap[[i]]),
+        variant = snapshot_variant
+      )
+    })
+  }
+}
