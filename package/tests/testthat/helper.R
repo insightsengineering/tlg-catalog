4262c5daac9e2f717b704d1485759776d7ec2a57
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-11-29 14:45:41 -0500

    Update ltg01 to fix legends, remove unneeded helper file

--- package/tests/testthat/helper.R
+++
@@ -1,4 +0,0 @@
-# Objects that need to be accessed by graph tests while recreating the graph snapshots
-mean_95_text <- "Mean +/- 95% CI"
-median_95_text <- "Median +/- 95% CI"
-median_80_text <- "Median +/- 80% CI"
