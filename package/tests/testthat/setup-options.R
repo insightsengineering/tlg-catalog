e2825a335f53b3b380e1e66b42cf6ca71ad715f7
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-01-31 18:08:16 +0100

    pass options as R_KNITR_OPTIONS envvar

--- package/tests/testthat/setup-options.R
+++ package/tests/testthat/setup-options.R
@@ -9,9 +9,18 @@ opts_partial_match_new <- list(
   warnPartialMatchAttr = TRUE
 )
 
+opts_to_set <- c(
+  opts_partial_match_new,
+  lifecycle_verbosity = "error",
+  warn = 2L,
+  pillar.width = Inf
+)
+
 if (isFALSE(getFromNamespace("on_cran", "testthat")()) && requireNamespace("withr", quietly = TRUE)) {
-  withr::local_options(
-    opts_partial_match_new,
+  withr::local_envvar(
+    # source: https://yihui.org/en/2023/10/opts-chunk/
+    # setting R options doesn't work as quarto creates a new R process
+    R_KNITR_OPTIONS = sprintf("knitr.chunk.R.options=%s", paste0(deparse(opts_to_set, width.cutoff = 500L), collapse = " ")),
     .local_envir = testthat::teardown_env()
   )
 }
