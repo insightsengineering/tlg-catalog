e8fa8e3acc42c37ab02724d5316e6f8d587dfb62
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-02-21 11:28:20 +0100

    rename extension

--- book/pattern-strip/_extensions/pattern-strip/pattern-strip.lua
+++ book/pattern-strip/_extensions/pattern-strip/pattern-strip.lua
@@ -3,8 +3,8 @@ local patterns = {}
 return {
   {
     Meta = function(meta)
-      if meta['regex-strip-pattern'] ~= nil then
-        for _, pattern in ipairs(meta['regex-strip-pattern']) do
+      if meta['pattern-strip-patterns'] ~= nil then
+        for _, pattern in ipairs(meta['pattern-strip-patterns']) do
           table.insert(patterns, pandoc.utils.stringify(pattern))
         end
       end
@@ -12,6 +12,11 @@ return {
   },
   {
     CodeBlock = function (el)
+      quarto.log.debug(el)
+      if not el.classes:includes("cell-code") then
+        return el
+      end
+
       local lines = pandoc.List()
       local code = el.text .. "\n"
       for line in code:gmatch("([^\n]*)\n") do
